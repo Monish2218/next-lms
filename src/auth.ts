2f346824e557431f85200d8eff46e46ea44cd6b3
Monish Agrawal <monish.21910923@viit.ac.in>
2024-10-24 22:17:10 +0530

    add prisma adapter for db connectivity

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,15 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
+// import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import prisma from "./lib/db"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google
+  ],
+  session: {
+    strategy: "jwt"
+  }
 })
